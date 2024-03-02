d5bfff131b47440064027a62bb375bdeb9b4fa6f
gitchobo622 <146044918+gitchobo622@users.noreply.github.com>
2024-03-02 15:24:52 +0900

    Update 00-text.py

--- 00-text.py
+++ 00-text.py
@@ -20,4 +20,4 @@ def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    run()
+    app.run(host='0.0.0.0', port=5000)
