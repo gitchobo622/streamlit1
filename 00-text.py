f9f5a4f3daff761d34e20adbd88d01d12a2c8c10
gitchobo622 <146044918+gitchobo622@users.noreply.github.com>
2024-03-02 15:23:11 +0900

    Update 00-text.py

--- 00-text.py
+++ 00-text.py
@@ -1,32 +1,23 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
 
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # 클라이언트의 IP 주소를 가져옵니다.
+        visitor_ip = self.client_address[0]
+        message = f"Your IP address is {visitor_ip}"
 
-st.title('데이터프레임 튜토리얼')
+        # 응답을 작성하고 반환합니다.
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
 
-# DataFrame 생성
-dataframe = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-})
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
 
-# DataFrame
-# use_container_width 기능은 데이터프레임을 컨테이너 크기에 확장할 때 사용합니다. (True/False)
-st.dataframe(dataframe, use_container_width=False)
-
-
-# 테이블(static)
-# DataFrame과는 다르게 interactive 한 UI 를 제공하지 않습니다.
-st.table(dataframe)
-
-
-# # 메트릭
-st.metric(label="온도", value="10°C", delta="1.2°C")
-st.metric(label="삼성전자", value="61,000 원", delta="-1,200 원")
-
-# 컬럼으로 영역을 나누어 표기한 경우
-col1, col2, col3 = st.columns(3)
-col1.metric(label="달러USD", value="1,228 원", delta="-12.00 원")
-col2.metric(label="일본JPY(100엔)", value="958.63 원", delta="-7.44 원")
-col3.metric(label="유럽연합EUR", value="1,335.82 원", delta="11.44 원")
+if __name__ == "__main__":
+    run()
